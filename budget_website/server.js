52943e55121bb340e963cc491ffd918fa4c5ce1e
sootymeerkat7 <97291962+sootymeerkat7@users.noreply.github.com>
2024-11-27 23:23:13 +0530

    Update server.js

--- budget_website/server.js
+++ budget_website/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dbConnect = require('./dbConnect')
 const app = express()
+require('dotenv').config();
 app.use(express.json())
 const path = require('path')
 const userRoute = require('./routes/usersRoute')
@@ -12,6 +13,7 @@ const port =process.env.PORT || 5000
 
 if(process.env.NODE_ENV === 'production')
 {
+     console.log("ENTRY");
      app.use('/' , express.static('client/build'))
 
      app.get('*' , (req, res)=>{
